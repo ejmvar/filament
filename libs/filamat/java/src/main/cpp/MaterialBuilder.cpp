815764fe6a7ff5be3b260fd01da8444902ca1b1d
Benjamin Doherty <bendoherty@google.com>
2018-11-30 12:38:09 -0800

    WIP implementing filamat-jni

---
+++ libs/filamat/java/src/main/cpp/MaterialBuilder.cpp
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <jni.h>
+
+#include <filamat/MaterialBuilder.h>
+
+#include <iostream>
+
+using namespace filamat;
+
+extern "C" JNIEXPORT jlong JNICALL
+Java_com_google_android_filament_filamat_MaterialBuilder_nCreateMaterialBuilder(JNIEnv *env,
+        jclass type) {
+    std::cout << "Creating a material builder\n";
+    return (jlong) new MaterialBuilder();
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_google_android_filament_filamat_MaterialBuilder_nDestroyMaterialBuilder(JNIEnv *env,
+        jclass type, jlong nativeBuilder) {
+    std::cout << "Destroying the material builder\n";
+    auto builder = (MaterialBuilder*) nativeBuilder;
+    delete builder;
+}
+
+extern "C" JNIEXPORT jlong JNICALL
+Java_com_google_android_filament_filamat_MaterialBuilder_nBuilderBuild(JNIEnv* env, jclass type,
+        jlong nativeBuilder) {
+    std::cout << "Build!";
+    auto builder = (MaterialBuilder*) nativeBuilder;
+    return (jlong) new Package(builder->build());
+}
+
+extern "C" JNIEXPORT jbyteArray JNICALL
+Java_com_google_android_filament_filamat_MaterialBuilder_nGetPackageBytes(JNIEnv* env, jclass type,
+        jlong nativePackage) {
+    auto package = (Package*) nativePackage;
+    auto size = jsize(package->getSize());
+    jbyteArray ret = env->NewByteArray(size);
+    auto data = (jbyte*) package->getData();
+    env->SetByteArrayRegion(ret, 0, size, data);
+    return ret;
+}
+
+extern "C" JNIEXPORT jboolean JNICALL
+Java_com_google_android_filament_filamat_MaterialBuilder_nGetPackageIsValid(JNIEnv* env,
+        jclass type, jlong nativePackage) {
+    auto* package = (Package*) nativePackage;
+    return jboolean(package->isValid());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_google_android_filament_filamat_MaterialBuilder_nDestroyPackage(JNIEnv* env, jclass type,
+        jlong nativePackage) {
+    Package* package = (Package*) nativePackage;
+    delete package;
+}
---
+++ libs/filamat/java/src/main/java/com/google/android/filament/filamat/MaterialBuilder.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (C) 2018 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.android.filament.filamat;
+
+import android.support.annotation.NonNull;
+import java.nio.ByteBuffer;
+
+public class MaterialBuilder {
+
+    private final BuilderFinalizer mFinalizer;
+    private final long mNativeObject;
+
+    public MaterialBuilder() {
+        mNativeObject = nCreateMaterialBuilder();
+        mFinalizer = new BuilderFinalizer(mNativeObject);
+    }
+
+    long getmNativeObject() {
+        if (mNativeObject == 0) {
+            throw new IllegalStateException("Calling method on destroyed MaterialBuilder");
+        }
+        return mNativeObject;
+    }
+
+    @NonNull
+    public MaterialPackage build() {
+        long nativePackage = nBuilderBuild(mNativeObject);
+        // todo: is it even possible to return 0?
+        if (nativePackage == 0) throw new IllegalStateException("Couldn't build Material");
+        byte[] data = nGetPackageBytes(nativePackage);
+        MaterialPackage result =
+                new MaterialPackage(ByteBuffer.wrap(data), nGetPackageIsValid(nativePackage));
+        nDestroyPackage(nativePackage);
+        return result;
+    }
+
+    private static class BuilderFinalizer {
+        private final long mNativeObject;
+
+        BuilderFinalizer(long nativeObject) {
+            mNativeObject = nativeObject;
+        }
+
+        @Override
+        public void finalize() {
+            try {
+                super.finalize();
+            } catch (Throwable t) { // Ignore
+            } finally {
+                nDestroyMaterialBuilder(mNativeObject);
+            }
+        }
+    }
+
+    private static native long nCreateMaterialBuilder();
+    private static native void nDestroyMaterialBuilder(long nativeBuilder);
+
+    private static native long nBuilderBuild(long nativeMaterialBuilder);
+    private static native byte[] nGetPackageBytes(long nativePackage);
+    private static native boolean nGetPackageIsValid(long nativePackage);
+    private static native void nDestroyPackage(long nativePackage);
+}
+
