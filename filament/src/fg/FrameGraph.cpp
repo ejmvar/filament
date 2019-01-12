8cb4eb7c7706cf6dc3dd6f4cfaa174afad7a6a3d
Mathias Agopian <mathias@google.com>
2019-01-11 16:38:49 -0800

    Add support for creating concrete resources
    - Tweak the API a bit.
    - We can now create concrete textures and render-targets

--- filament/src/fg/FrameGraph.cpp
+++ filament/src/fg/FrameGraph.cpp
@@ -16,6 +16,12 @@
 
 #include "FrameGraph.h"
 
+#include "FrameGraphPassResources.h"
+
+#include "driver/Driver.h"
+#include "driver/Handle.h"
+#include "driver/CommandStream.h"
+
 #include <utils/Panic.h>
 #include <utils/Log.h>
 
@@ -31,15 +37,76 @@ namespace fg {
 
 struct Resource {
     explicit Resource(const char* name) noexcept : name(name) { }
+
+    ~Resource() noexcept;
+
     // constants
     const char* const name;         // for debugging
+
     // computed during compile()
     PassNode* writer = nullptr;     // last writer to this resource
     PassNode* first = nullptr;      // pass that needs to instantiate the resource
     PassNode* last = nullptr;       // pass that can destroy the resource
     uint32_t writerCount = 0;       // # of passes writing to this resource
     uint32_t readerCount = 0;       // # of passes reading from this resource
+
+    // concrete resource
+    void create(driver::DriverApi& driver) noexcept;
+    void destroy(driver::DriverApi& driver) noexcept;
+    FrameGraphResource::Descriptor desc;
+    FrameGraph::Builder::RWFlags readFlags = 0;
+    FrameGraph::Builder::RWFlags writeFlags = 0;
+    Handle<HwTexture> textures[2];  // color, depth
+    Handle<HwRenderTarget> target;
 };
+
+Resource::~Resource() noexcept {
+    assert(!textures[0]);
+    assert(!textures[1]);
+    assert(!target);
+}
+
+void Resource::create(driver::DriverApi& driver) noexcept {
+    if (readerCount) {
+        assert(readFlags);
+        if (readFlags & FrameGraph::Builder::COLOR) {
+            textures[0] = driver.createTexture(desc.type, desc.levels,
+                    desc.format, 1,
+                    desc.width, desc.height, desc.depth,
+                    driver::TextureUsage::COLOR_ATTACHMENT);
+        }
+        if (readFlags & FrameGraph::Builder::DEPTH) {
+            textures[1] = driver.createTexture(desc.type, desc.levels,
+                    driver::TextureFormat::DEPTH24, 1,
+                    desc.width, desc.height, desc.depth,
+                    driver::TextureUsage::DEPTH_ATTACHMENT);
+        }
+    }
+    if (writerCount) {
+        assert(writeFlags);
+        uint32_t attachments = 0;
+        if (writeFlags & FrameGraph::Builder::COLOR) {
+            attachments |= uint32_t(driver::TargetBufferFlags::COLOR);
+        }
+        if (writeFlags & FrameGraph::Builder::DEPTH) {
+            attachments |= driver::TargetBufferFlags::DEPTH;
+        }
+        target = driver.createRenderTarget(driver::TargetBufferFlags(attachments),
+                desc.width, desc.height, desc.samples, desc.format,
+                { textures[0] }, { textures[1] }, {});
+    }
+}
+
+void Resource::destroy(driver::DriverApi& driver) noexcept {
+    for (auto& texture : textures) {
+        if (texture) {
+            driver.destroyTexture(texture);
+        }
+    }
+    if (target) {
+        driver.destroyRenderTarget(target);
+    }
+}
 
 struct ResourceNode {
     ResourceNode(const char* name, uint16_t index) noexcept : name(name), index(index) { }
@@ -52,6 +119,9 @@ struct ResourceNode {
 
     // updated by the builder
     uint16_t version = 0;
+    FrameGraphResource::Descriptor desc;
+    FrameGraph::Builder::RWFlags readFlags = 0;
+    FrameGraph::Builder::RWFlags writeFlags= 0;
 
     // set during compile()
     Resource* resource = nullptr;
@@ -80,7 +150,7 @@ struct PassNode {
 
     ~PassNode() { delete base; }
 
-    // for FrameGraphBuilder
+    // for Builder
     void read(ResourceNode const& resource) {
         // just record that we're reading from this resource (at the given version)
         reads.push_back({ resource.index, resource.version });
@@ -121,64 +191,35 @@ FrameGraphPassExecutor::~FrameGraphPassExecutor() = default;
 
 // ------------------------------------------------------------------------------------------------
 
-FrameGraphBuilder::FrameGraphBuilder(FrameGraph& fg, PassNode& pass) noexcept
+FrameGraph::Builder::Builder(FrameGraph& fg, PassNode& pass) noexcept
     : mFrameGraph(fg), mPass(pass) {
 }
 
-ResourceNode* FrameGraphBuilder::getResource(FrameGraphResource input) {
-    FrameGraph& frameGraph = mFrameGraph;
-    auto& registry = frameGraph.mResourceNodes;
-
-    if (!ASSERT_POSTCONDITION_NON_FATAL(input.isValid(),
-            "using an uninitialized resource handle")) {
-        return nullptr;
-    }
-
-    assert(input.index < registry.size());
-    auto& resource = registry[input.index];
-
-    if (!ASSERT_POSTCONDITION_NON_FATAL(input.version == resource.version,
-            "using an invalid resource handle (version=%u) for resource=\"%s\" (id=%u, version=%u)",
-            input.version, resource.name, resource.index, resource.version)) {
-        return nullptr;
-    }
-
-    return &resource;
-}
-
-FrameGraphResource FrameGraphBuilder::createTexture(
-        const char* name, CreateFlags flags, FrameGraphResource::TextureDesc const& desc) noexcept {
+FrameGraphResource FrameGraph::Builder::createTexture(
+        const char* name, FrameGraphResource::Descriptor const& desc) noexcept {
     FrameGraph& frameGraph = mFrameGraph;
     ResourceNode& resource = frameGraph.createResource(name);
-    switch (flags) {
-        case UNKNOWN:
-            // we just create a resource, but we don't use it in this pass
-            break;
-        case READ:
-            mPass.read(resource);
-            break;
-        case WRITE:
-            mPass.write(resource);
-            break;
-    }
+    resource.desc = desc;
     return { resource.index, resource.version };
 }
 
-FrameGraphResource FrameGraphBuilder::read(FrameGraphResource const& input) {
-    ResourceNode* resource = getResource(input);
+FrameGraphResource FrameGraph::Builder::read(FrameGraphResource const& input, RWFlags readFlags) {
+    ResourceNode* resource = mFrameGraph.getResource(input);
     if (!resource) {
         return {};
     }
+    resource->readFlags |= readFlags;
     mPass.read(*resource);
     return input;
 }
 
-FrameGraphResource FrameGraphBuilder::write(FrameGraphResource const& output) {
-    ResourceNode* resource = getResource(output);
+FrameGraphResource FrameGraph::Builder::write(FrameGraphResource const& output, RWFlags writeFlags) {
+    ResourceNode* resource = mFrameGraph.getResource(output);
     if (!resource) {
         return {};
     }
 
+    resource->writeFlags |= writeFlags;
     mPass.write(*resource);
 
     /*
@@ -196,6 +237,41 @@ FrameGraphResource FrameGraphBuilder::write(FrameGraphResource const& output) {
      */
 
     return { resource->index, resource->version };
+}
+
+// ------------------------------------------------------------------------------------------------
+
+FrameGraphPassResources::FrameGraphPassResources(FrameGraph& fg, fg::PassNode const& pass) noexcept
+    : mFrameGraph(fg), mPass(pass) {
+}
+
+Handle<HwTexture> FrameGraphPassResources::getTexture(
+        FrameGraphResource r, driver::TextureUsage attachment) const {
+    // TODO: we should check that this FrameGraphResource is indeed used by this pass
+    (void)mPass; // suppress unused warning
+    Resource const* const pResource = mFrameGraph.mResourceNodes[r.index].resource;
+    assert(pResource);
+
+    switch (attachment) {
+        case driver::TextureUsage::DEFAULT:
+            if (pResource->readFlags == FrameGraph::Builder::DEPTH) {
+                return pResource->textures[1];
+            }
+            [[clang::fallthrough]];
+        case driver::TextureUsage::COLOR_ATTACHMENT:
+            return pResource->textures[0];
+
+        case driver::TextureUsage::DEPTH_ATTACHMENT:
+            return pResource->textures[1];
+    }
+}
+
+Handle<HwRenderTarget> FrameGraphPassResources::getRenderTarget(FrameGraphResource r) const {
+    // TODO: we should check that this FrameGraphResource is indeed used by this pass
+    (void)mPass; // suppress unused warning
+    Resource const* const pResource = mFrameGraph.mResourceNodes[r.index].resource;
+    assert(pResource);
+    return pResource->target;
 }
 
 // ------------------------------------------------------------------------------------------------
@@ -221,10 +297,10 @@ void FrameGraph::present(FrameGraphResource input) {
     struct Dummy {
     };
     addPass<Dummy>("Present",
-            [&input](FrameGraphBuilder& builder, Dummy& data) {
+            [&input](Builder& builder, Dummy& data) {
                 builder.read(input);
             },
-            [](FrameGraphPassResources const& resources, Dummy const& data) {
+            [](FrameGraphPassResources const& resources, Dummy const& data, driver::DriverApi&) {
             });
 }
 
@@ -242,19 +318,48 @@ ResourceNode& FrameGraph::createResource(const char* name) noexcept {
     return registry.back();
 }
 
-FrameGraph& FrameGraph::compile() noexcept {
+
+ResourceNode* FrameGraph::getResource(FrameGraphResource r) {
     auto& registry = mResourceNodes;
-    mResourceRegistry.reserve(registry.size());
+
+    if (!ASSERT_POSTCONDITION_NON_FATAL(r.isValid(),
+            "using an uninitialized resource handle")) {
+        return nullptr;
+    }
+
+    assert(r.index < registry.size());
+    auto& resource = registry[r.index];
+
+    if (!ASSERT_POSTCONDITION_NON_FATAL(r.version == resource.version,
+            "using an invalid resource handle (version=%u) for resource=\"%s\" (id=%u, version=%u)",
+            r.version, resource.name, resource.index, resource.version)) {
+        return nullptr;
+    }
+
+    return &resource;
+}
+FrameGraphResource::Descriptor* FrameGraph::getTextureDesc(FrameGraphResource r) {
+    ResourceNode* node = getResource(r);
+    return node ? &node->desc : nullptr;
+}
+
+FrameGraph& FrameGraph::compile() noexcept {
+    auto& resourceNodes = mResourceNodes;
+    auto& resourceRegistry = mResourceRegistry;
+    resourceRegistry.reserve(resourceNodes.size());
 
     // create the sub-resources
-    for (ResourceNode& node : registry) {
-        mResourceRegistry.emplace_back(node.name);
-        node.resource = &mResourceRegistry.back();
+    for (ResourceNode& node : resourceNodes) {
+        resourceRegistry.emplace_back(node.name);
+        node.resource = &resourceRegistry.back();
+        node.resource->desc = node.desc;
+        node.resource->readFlags = node.readFlags;
+        node.resource->writeFlags = node.writeFlags;
     }
 
     // remap them
     for (auto const& alias : mAliases) {
-        registry[alias.to.index].resource = registry[alias.from.index].resource;
+        resourceNodes[alias.to.index].resource = resourceNodes[alias.from.index].resource;
     }
 
     for (PassNode& pass : mPassNodes) {
@@ -263,7 +368,7 @@ FrameGraph& FrameGraph::compile() noexcept {
 
         // compute resources reference counts (i.e. resources we're reading from), and first/last users
         for (FrameGraphResource resource : pass.reads) {
-            Resource* subResource = registry[resource.index].resource;
+            Resource* subResource = resourceNodes[resource.index].resource;
 
             // add a reference for each pass that reads from this resource
             subResource->readerCount++;
@@ -277,16 +382,24 @@ FrameGraph& FrameGraph::compile() noexcept {
 
         // set the writers
         for (FrameGraphResource resource : pass.writes) {
-            Resource* subResource = registry[resource.index].resource;
+            Resource* subResource = resourceNodes[resource.index].resource;
             subResource->writer = &pass;
+
+            // add a reference for each pass that writes to this resource
             subResource->writerCount++;
+
+            // figure out which is the first pass to need this resource
+            subResource->first = subResource->first ? subResource->first : &pass;
+
+            // figure out which is the last pass to need this resource
+            subResource->last = &pass;
         }
     }
 
     // cull passes and resources...
     std::vector<Resource*> stack;
-    stack.reserve(registry.size());
-    for (Resource& resource : mResourceRegistry) {
+    stack.reserve(resourceNodes.size());
+    for (Resource& resource : resourceRegistry) {
         if (resource.readerCount == 0) {
             stack.push_back(&resource);
         }
@@ -315,7 +428,7 @@ FrameGraph& FrameGraph::compile() noexcept {
             // this pass is culled
             auto const& reads = writer->reads;
             for (FrameGraphResource resource : reads) {
-                Resource* r = registry[resource.index].resource;
+                Resource* r = resourceNodes[resource.index].resource;
                 if (--r->readerCount == 0) {
                     stack.push_back(r);
                 }
@@ -323,8 +436,8 @@ FrameGraph& FrameGraph::compile() noexcept {
         }
     }
 
-    for (size_t index = 0, c = mResourceRegistry.size() ; index < c ; index++) {
-        auto& resource = mResourceRegistry[index];
+    for (size_t index = 0, c = resourceRegistry.size() ; index < c ; index++) {
+        auto& resource = resourceRegistry[index];
         assert(!resource.first == !resource.last);
         if (resource.readerCount && resource.first && resource.last) {
             resource.first->devirtualize.push_back((uint16_t)index);
@@ -335,26 +448,32 @@ FrameGraph& FrameGraph::compile() noexcept {
     return *this;
 }
 
-void FrameGraph::execute() noexcept {
-    auto const& resources = mResources;
+void FrameGraph::execute(driver::DriverApi& driver) noexcept {
+    auto& resourceRegistry = mResourceRegistry;
     for (PassNode const& node : mPassNodes) {
         if (!node.refCount) continue;
+        assert(node.base);
 
+        FrameGraphPassResources resources(*this, node);
+
+        // create concrete resources
         for (size_t id : node.devirtualize) {
-            // TODO: create concrete resources
+            resourceRegistry[id].create(driver);
         }
 
-        assert(node.base);
-        node.base->execute(resources);
+        // execute the pass
+        node.base->execute(resources, driver);
 
+        // destroy concrete resources
         for (uint32_t id : node.destroy) {
-            // TODO: delete concrete resources
+            resourceRegistry[id].destroy(driver);
         }
     }
 
     // reset the frame graph state
     mPassNodes.clear();
     mResourceNodes.clear();
+    mResourceRegistry.clear();
     mAliases.clear();
 }
 
